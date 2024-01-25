ae220a774142dd20e3bee8bd1f43a6440e4f240d
Satish Kumar <88663964+Satish7800@users.noreply.github.com>
2024-01-25 11:15:44 +0530

    Remove alert and console
    Remove some code in app.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ function App() {
   const [data,setData]=useState("Satish");
   return (
     <div className="App" >
-        console.warn(data);
-        alert(data);
+      
         <h1>{data}</h1>
         <button class="btn btn-primary sm" onClick={()=>setData("Raj")}>Update</button>
 
