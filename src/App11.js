053718b6e6fa617a9c905862dc18159851af7148
Satish Kumar <88663964+Satish7800@users.noreply.github.com>
2024-01-31 11:28:04 +0530

    ch

--- src/App11.js
+++ src/App11.js
@@ -7,7 +7,7 @@ import React, { useState,useEffect } from 'react';
 function App() {
   const [count,setCount]=useState(0);
   React.useEffect(()=>{
-    console.log('useEffect');
+    console.log('useEffects');
   });
  
   return (
