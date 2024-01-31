3b037476be5155c0c7b1458408ebbaa0e6ad8e56
Satish Kumar <88663964+Satish7800@users.noreply.github.com>
2024-01-31 10:52:12 +0530

    ef

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 // import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';  
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 function App() {
   React.useEffect(()=>{
