9b70a812577bd4df671a35ca6b95b328bf576794
Satish Kumar <88663964+Satish7800@users.noreply.github.com>
2024-02-01 11:51:17 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 // import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import React, { useState,useEffect } from 'react';
+import { Button } from 'react-bootstrap';
 import User from './User';
 
 function App() {
   const [count,setCount]=useState(100);
   const [data,setData]=useState(10);
  
+  
   return (
     <div className="App" >
         <User count={count} data={data}/>
-        <button className="btn btn-primary" onClick={()=>setCount(count+1)}>Update</button><br></br>
-        <button className="btn btn-primary" onClick={()=>setData(data+1)}>Data</button>
+        <Button  onClick={()=>setCount(count+1)}>Update</Button><br></br>
+        <Button className="btn btn-primary" onClick={()=>setData(data+1)}>Data</Button>
     </div>
   );
 }
